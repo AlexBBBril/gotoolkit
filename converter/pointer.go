355808b3957f653eca2301eef5af60fcc90fa98f
Alex <20801994+AlexBBBril@users.noreply.github.com>
2020-04-29 17:07:56 +0300

    Converter primitives to pointer

---
+++ converter/pointer.go
@@ -0,0 +1,96 @@
+package convert
+
+// StringPointer convert string to string pointer
+func StringPointer(s string) *string {
+	return &s
+}
+
+// IntPointer convert int to int pointer
+func IntPointer(i int) *int {
+	return &i
+}
+
+// Int8Pointer convert int8 to int8 pointer
+func Int8Pointer(i int8) *int8 {
+	return &i
+}
+
+// Int16Pointer convert int16 to int16 pointer
+func Int16Pointer(i int16) *int16 {
+	return &i
+}
+
+// Int32Pointer convert int32 to int32 pointer
+func Int32Pointer(i int32) *int32 {
+	return &i
+}
+
+// Int64Pointer convert int64 to int64 pointer
+func Int64Pointer(i int64) *int64 {
+	return &i
+}
+
+// UintPointer convert uint to uint pointer
+func UintPointer(u uint) *uint {
+	return &u
+}
+
+// Uint8Pointer convert uint8 to uint8 pointer
+func Uint8Pointer(u uint8) *uint8 {
+	return &u
+}
+
+// Uint16Pointer convert uint16 to uint16 pointer
+func Uint16Pointer(u uint16) *uint16 {
+	return &u
+}
+
+// Uint32Pointer convert uint32 to uint32 pointer
+func Uint32Pointer(u uint32) *uint32 {
+	return &u
+}
+
+// Uint64Pointer convert uint64 to uint64 pointer
+func Uint64Pointer(u uint64) *uint64 {
+	return &u
+}
+
+// UintptrPointer convert uint64 to uint64 pointer
+func UintptrPointer(u uintptr) *uintptr {
+	return &u
+}
+
+// BytePointer convert byte to byte pointer
+func BytePointer(b byte) *byte {
+	return &b
+}
+
+// RunePointer convert byte to byte pointer
+func RunePointer(r rune) *rune {
+	return &r
+}
+
+// Float32Pointer convert float32 to float32 pointer
+func Float32Pointer(f float32) *float32 {
+	return &f
+}
+
+// Float64Pointer convert float64 to float64 pointer
+func Float64Pointer(f float64) *float64 {
+	return &f
+}
+
+// Complex64Pointer convert complex64 to complex64 pointer
+func Complex64Pointer(c complex64) *complex64 {
+	return &c
+}
+
+// Complex128Pointer convert complex128 to complex128 pointer
+func Complex128Pointer(f complex128) *complex128 {
+	return &f
+}
+
+// BooleanPointer convert bool to bool pointer
+func BooleanPointer(b bool) *bool {
+	return &b
+}
